187aa8ad38d3fee09f7e0c0f7d1b6ce12cf1420d
Gregory Pike <greg@shinyads.com>
2013-06-14 18:11:40 -0400

    Prevent error when no elements are returned

--- jquery.simplescroll.js
+++ jquery.simplescroll.js
@@ -7,7 +7,7 @@ if (typeof $.fn.simpleScroll == "undefined")
 	if (!duration)
 		duration = 1000;
 
-	if (typeof this.offset != "undefined")
+	if (typeof this.offset != "undefined" && this.offset() !== null)
 		return $('html, body').animate({scrollTop: this.offset().top}, duration);
 
 	return false;
